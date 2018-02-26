fe4f474ec8d2ad1622d22ce5f48217d2b01734cd
Gilles Crettenand <gilles.crettenand@liip.ch>
2018-02-26 10:46:03 +0100

    first blood

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jekyll / GitHub pages project template for ANDco
+
+To create a new project using this template, do the following:
+
+    pip install cookiecutter pip-tools
+    cookiecutter gh:StudioANDco/jekyll-andco-template
+
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+
+if __name__ == '__main__':
+    os.system('git init .')
+    os.system('npm install')
+    os.system('node_modules/.bin/kanbasu _sass/')
+    os.system('git add .')
+    os.system('git commit -am "first blood"')
