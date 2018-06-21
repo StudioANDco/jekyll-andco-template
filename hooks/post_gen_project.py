d7221207215727caa6eb0f37bf09cc4e80e68708
Gilles Crettenand <gilles.crettenand@liip.ch>
2018-06-21 16:35:45 +0200

    add pre-commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,7 @@ import os
 
 if __name__ == '__main__':
     os.system('git init .')
+    os.system('pre-commit install')
     os.system('npm install')
     os.system('node_modules/.bin/kanbasu _sass/')
     os.system('git add .')
